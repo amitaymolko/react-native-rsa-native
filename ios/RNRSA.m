87b719231aaa132b06d67d7bb6d3e19863233303
AlexanderPinkerton <alexpinkerton88@gmail.com>
2017-12-19 19:27:56 -0500

    Should be exporting 64 keychain functions on ios now

--- ios/RNRSA.m
+++ ios/RNRSA.m
@@ -128,10 +128,24 @@ RCT_EXPORT_METHOD(encrypt:(NSString *)message withKeyTag:(NSString *)keyTag reso
     resolve(encodedMessage);
 }
 
+RCT_EXPORT_METHOD(encrypt64:(NSString *)message withKeyTag:(NSString *)keyTag resolve:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    RSANative *rsa = [[RSANative alloc] initWithKeyTag:keyTag];
+    NSString *encodedMessage = [rsa encrypt64:message];
+    resolve(encodedMessage);
+}
+
 RCT_EXPORT_METHOD(decrypt:(NSString *)encodedMessage withKeyTag:(NSString *)keyTag resolve:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     RSANative *rsa = [[RSANative alloc] initWithKeyTag:keyTag];
     NSString *message = [rsa decrypt:encodedMessage];
+    resolve(message);
+}
+
+RCT_EXPORT_METHOD(decrypt64:(NSString *)encodedMessage withKeyTag:(NSString *)keyTag resolve:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    RSANative *rsa = [[RSANative alloc] initWithKeyTag:keyTag];
+    NSString *message = [rsa decrypt64:encodedMessage];
     resolve(message);
 }
 
@@ -142,10 +156,24 @@ RCT_EXPORT_METHOD(sign:(NSString *)message withKeyTag:(NSString *)keyTag resolve
     resolve(signature);
 }
 
+RCT_EXPORT_METHOD(sign64:(NSString *)message withKeyTag:(NSString *)keyTag resolve:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    RSANative *rsa = [[RSANative alloc] initWithKeyTag:keyTag];
+    NSString *signature = [rsa sign64:message];
+    resolve(signature);
+}
+
 RCT_EXPORT_METHOD(verify:(NSString *)signature withMessage:(NSString *)message andKeyTag:(NSString *)keyTag resolve:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     RSANative *rsa = [[RSANative alloc] initWithKeyTag:keyTag];
     BOOL valid = [rsa verify:signature withMessage:message];
+    resolve(@(valid));
+}
+
+RCT_EXPORT_METHOD(verify64:(NSString *)signature withMessage:(NSString *)message andKeyTag:(NSString *)keyTag resolve:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    RSANative *rsa = [[RSANative alloc] initWithKeyTag:keyTag];
+    BOOL valid = [rsa verify64:signature withMessage:message];
     resolve(@(valid));
 }
 
