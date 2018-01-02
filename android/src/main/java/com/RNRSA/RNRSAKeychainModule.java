c0e035e394d2ac5f4e2a974f0ea334c906899b06
AlexanderPinkerton <alexpinkerton88@gmail.com>
2018-01-02 12:14:04 -0500

    Now should print stacktrace

--- android/src/main/java/com/RNRSA/RNRSAKeychainModule.java
+++ android/src/main/java/com/RNRSA/RNRSAKeychainModule.java
@@ -14,163 +14,161 @@ import java.security.NoSuchAlgorithmException;
 
 public class RNRSAKeychainModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNRSAKeychainModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RNRSAKeychain";
-  }
-
-  @ReactMethod
-  public void generate(String keyTag, Promise promise)  {
-    WritableNativeMap keys = new WritableNativeMap();
-
-    try {
-        RSA rsa = new RSA();
-        rsa.generate(keyTag);
-        keys.putString("public",  rsa.getPublicKey());
-        promise.resolve(keys);
-    } catch(NoSuchAlgorithmException e) {
-      promise.reject("Error", e.getMessage(), e.printStackTrace());
-    } catch(Exception e) {
-      promise.reject("Error", e.getMessage(), e.printStackTrace());
+    public RNRSAKeychainModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
     }
-  }
 
-  @ReactMethod
-  public void getPublicKey(String keyTag, Promise promise) {
-    try {
-        RSA rsa = new RSA(keyTag);
-        String publicKey = rsa.getPublicKey();
-        promise.resolve(publicKey);
-    } catch(NoSuchAlgorithmException e) {
-      promise.reject("Error", e.getMessage(), e.printStackTrace());
-    } catch(Exception e) {
-      promise.reject("Error", e.getMessage(), e.printStackTrace());
+    @Override
+    public String getName() {
+        return "RNRSAKeychain";
     }
-  }
-
-  @ReactMethod
-  public void deletePrivateKey(String keyTag, Promise promise)  {
-    try {
-        RSA rsa = new RSA(keyTag);
-        rsa.deletePrivateKey();
-        promise.resolve(1);
-    } catch(NoSuchAlgorithmException e) {
-      promise.reject("Error", e.getMessage(), e.printStackTrace());
-    } catch(Exception e) {
-      promise.reject("Error", e.getMessage(), e.printStackTrace());
-    }
-  }
-
-  @ReactMethod
-  public void encrypt(String message, String keyTag, Promise promise)  {
-
-      try {
-          RSA rsa = new RSA(keyTag);
-          String encodedMessage = rsa.encrypt(message);
-          promise.resolve(encodedMessage);
-      } catch(Exception e) {
-          promise.reject("Error", e.getMessage(), e.printStackTrace());
-      }
-  }
-
-  @ReactMethod
-  public void encrypt64(String message, String keyTag, Promise promise)  {
-
-      try {
-          RSA rsa = new RSA(keyTag);
-          String encodedMessage = rsa.encrypt64(message);
-          promise.resolve(encodedMessage);
-      } catch(Exception e) {
-          promise.reject("Error", e.getMessage(), e.printStackTrace());
-      }
-  }
-
-
-  @ReactMethod
-  public void decrypt(String encodedMessage, String keyTag, Promise promise)  {
-
-      try {
-          RSA rsa = new RSA(keyTag);
-          String message = rsa.decrypt(encodedMessage);
-          promise.resolve(message);
-
-      } catch(Exception e) {
-          promise.reject("Error", e.getMessage(), e.printStackTrace());
-      }
-  }
-
-  @ReactMethod
-  public void decrypt64(String encodedMessage, String keyTag, Promise promise)  {
-
-      try {
-          RSA rsa = new RSA(keyTag);
-          String message = rsa.decrypt64(encodedMessage);
-          promise.resolve(message);
-
-      } catch(Exception e) {
-          promise.reject("Error", e.getMessage(), e.printStackTrace());
-      }
-  }
 
     @ReactMethod
-    public void sign(String message, String keyTag, Promise promise)  {
+    public void generate(String keyTag, Promise promise) {
+        WritableNativeMap keys = new WritableNativeMap();
+
+        try {
+            RSA rsa = new RSA();
+            rsa.generate(keyTag);
+            keys.putString("public", rsa.getPublicKey());
+            promise.resolve(keys);
+        } catch (NoSuchAlgorithmException e) {
+
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        }
+    }
+
+    @ReactMethod
+    public void getPublicKey(String keyTag, Promise promise) {
+        try {
+            RSA rsa = new RSA(keyTag);
+            String publicKey = rsa.getPublicKey();
+            promise.resolve(publicKey);
+        } catch (NoSuchAlgorithmException e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        }
+    }
+
+    @ReactMethod
+    public void deletePrivateKey(String keyTag, Promise promise) {
+        try {
+            RSA rsa = new RSA(keyTag);
+            rsa.deletePrivateKey();
+            promise.resolve(1);
+        } catch (NoSuchAlgorithmException e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        }
+    }
+
+    @ReactMethod
+    public void encrypt(String message, String keyTag, Promise promise) {
+
+        try {
+            RSA rsa = new RSA(keyTag);
+            String encodedMessage = rsa.encrypt(message);
+            promise.resolve(encodedMessage);
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        }
+    }
+
+    @ReactMethod
+    public void encrypt64(String message, String keyTag, Promise promise) {
+
+        try {
+            RSA rsa = new RSA(keyTag);
+            String encodedMessage = rsa.encrypt64(message);
+            promise.resolve(encodedMessage);
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        }
+    }
+
+    @ReactMethod
+    public void decrypt(String encodedMessage, String keyTag, Promise promise) {
+
+        try {
+            RSA rsa = new RSA(keyTag);
+            String message = rsa.decrypt(encodedMessage);
+            promise.resolve(message);
+
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        }
+    }
+
+    @ReactMethod
+    public void decrypt64(String encodedMessage, String keyTag, Promise promise) {
+
+        try {
+            RSA rsa = new RSA(keyTag);
+            String message = rsa.decrypt64(encodedMessage);
+            promise.resolve(message);
+
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
+        }
+    }
+
+    @ReactMethod
+    public void sign(String message, String keyTag, Promise promise) {
 
         try {
             RSA rsa = new RSA(keyTag);
             String signature = rsa.sign(message);
             promise.resolve(signature);
 
-        } catch(Exception e) {
-            promise.reject("Error", e.getMessage(), e.printStackTrace());
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
         }
     }
 
     @ReactMethod
-    public void sign64(String message, String keyTag, Promise promise)  {
+    public void sign64(String message, String keyTag, Promise promise) {
 
         try {
             RSA rsa = new RSA(keyTag);
             String signature = rsa.sign64(message);
             promise.resolve(signature);
 
-        } catch(Exception e) {
-            promise.reject("Error", e.getMessage(), e.printStackTrace());
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
         }
     }
 
     @ReactMethod
-    public void verify(String signature, String message, String keyTag, Promise promise)  {
+    public void verify(String signature, String message, String keyTag, Promise promise) {
 
         try {
             RSA rsa = new RSA(keyTag);
             boolean verified = rsa.verify(signature, message);
             promise.resolve(verified);
 
-        } catch(Exception e) {
-            promise.reject("Error", e.getMessage(), e.printStackTrace());
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
         }
     }
 
     @ReactMethod
-    public void verify64(String signature, String message, String keyTag, Promise promise)  {
+    public void verify64(String signature, String message, String keyTag, Promise promise) {
 
         try {
             RSA rsa = new RSA(keyTag);
             boolean verified = rsa.verify64(signature, message);
             promise.resolve(verified);
 
-        } catch(Exception e) {
-            promise.reject("Error", e.getMessage(), e.printStackTrace());
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage() + Log.getStackTraceString(e));
         }
     }
-
-
 
 }
