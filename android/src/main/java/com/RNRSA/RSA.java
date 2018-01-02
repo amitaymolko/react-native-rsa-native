2dbf611b5ddb6187d14073c56b93f10177038bf9
AlexanderPinkerton <alexpinkerton88@gmail.com>
2018-01-02 15:56:13 -0500

    more trial and error

--- android/src/main/java/com/RNRSA/RSA.java
+++ android/src/main/java/com/RNRSA/RSA.java
@@ -75,7 +75,7 @@ public class RSA {
     public RSA(String keyTag) throws KeyStoreException, UnrecoverableEntryException, NoSuchAlgorithmException, IOException, CertificateException {
         this.keyTag = keyTag;
         this.loadFromKeystore();
-        Log.v("VIDA", getPrivateKey());
+
     }
 
     public String getPublicKey() throws IOException {
@@ -275,7 +275,7 @@ public class RSA {
                 PURPOSE_ENCRYPT | PURPOSE_DECRYPT | PURPOSE_SIGN | PURPOSE_VERIFY
         )
                 .setDigests(KeyProperties.DIGEST_SHA512, KeyProperties.DIGEST_SHA1)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_OAEP)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE, KeyProperties.ENCRYPTION_PADDING_RSA_OAEP)
                 .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
                 .setKeySize(2048)
                 .build());
