0664c4bfb9bf21c095b35e7975e18c2d7edd63fe
Manuel Alabor <manuel@alabor.me>
2018-06-13 12:57:41 +0200

    Add Type Definitions for TypeScript

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare module 'react-native-rsa-native' {
+	interface PublicKey {
+		public: string;
+	}
+
+	interface KeyPair extends PublicKey {
+		private: string;
+	}
+
+	namespace RSA {
+		export function generateKeys(keySize: number): Promise<KeyPair>;
+		export function encrypt(data: string, key: string): Promise<string>;
+		export function decrypt(data: string, key: string): Promise<string>;
+		export function sign(data: string, key: string): Promise<string>;
+		export function verify(data: string, secretToVerify: string, key: string): Promise<boolean>;
+	}
+
+	namespace RSAKeychain {
+		export function generateKeys(keyTag: string, keySize: number): Promise<PublicKey>;
+		export function deletePrivateKey(keyTag: string): Promise<boolean>;
+		export function encrypt(data: string, keyTag: string): Promise<string>;
+		export function decrypt(data: string, keyTag: string): Promise<string>;
+		export function sign(data: string, keyTag: string): Promise<string>;
+		export function verify(data: string, secretToVerify: string, keyTag: string): Promise<boolean>;
+		export function getPublicKey(keyTag: string): Promise<string | undefined>;
+	}
+
+	export { RSA, RSAKeychain };
+}
