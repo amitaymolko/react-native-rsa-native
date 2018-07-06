9123977802d4ef3d8b6da201b5453061bceea9d0
Manuel Alabor <manuel@alabor.me>
2018-07-06 09:59:57 +0200

    Run Expensive Operations in Additonal Thread
    Key Operations are expensive and might freeze the UI thread. This change
    puts those operations in a separate thread for Android.

--- android/src/main/java/com/RNRSA/RNRSAKeychainModule.java
+++ android/src/main/java/com/RNRSA/RNRSAKeychainModule.java
@@ -1,6 +1,7 @@
 
 package com.RNRSA;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
@@ -27,147 +28,193 @@ public class RNRSAKeychainModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void generate(String keyTag, Promise promise)  {
+  public void generate(final String keyTag, final Promise promise) {
     this.generateKeys(keyTag, 2048, promise);
   }
 
   @ReactMethod
-  public void generateKeys(String keyTag, int keySize, Promise promise)  {
-    WritableNativeMap keys = new WritableNativeMap();
+  public void generateKeys(final String keyTag, final int keySize, final Promise promise) {
+    final ReactApplicationContext reactContext = this.reactContext;
 
-    try {
-        RSA rsa = new RSA();
-        rsa.generate(keyTag, keySize, this.reactContext);
-        keys.putString("public",  rsa.getPublicKey());
-        promise.resolve(keys);
-    } catch(NoSuchAlgorithmException e) {
-      promise.reject("Error", e.getMessage());
-    } catch(Exception e) {
-      promise.reject("Error", e.getMessage());
-    }
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        WritableNativeMap keys = new WritableNativeMap();
+
+        try {
+          RSA rsa = new RSA();
+          rsa.generate(keyTag, keySize, reactContext);
+          keys.putString("public", rsa.getPublicKey());
+          promise.resolve(keys);
+        } catch (NoSuchAlgorithmException e) {
+          promise.reject("Error", e.getMessage());
+        } catch (Exception e) {
+          promise.reject("Error", e.getMessage());
+        }
+      }
+    });
   }
 
   @ReactMethod
-  public void deletePrivateKey(String keyTag, Promise promise)  {
-    try {
-        RSA rsa = new RSA(keyTag);
-        rsa.deletePrivateKey();
-        promise.resolve(1);
-    } catch(NoSuchAlgorithmException e) {
-      promise.reject("Error", e.getMessage());
-    } catch(Exception e) {
-      promise.reject("Error", e.getMessage());
-    }
+  public void deletePrivateKey(final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          RSA rsa = new RSA(keyTag);
+          rsa.deletePrivateKey();
+          promise.resolve(1);
+        } catch (NoSuchAlgorithmException e) {
+          promise.reject("Error", e.getMessage());
+        } catch (Exception e) {
+          promise.reject("Error", e.getMessage());
+        }
+      }
+    });
   }
 
   @ReactMethod
-  public void encrypt(String message, String keyTag, Promise promise)  {
-
-      try {
+  public void encrypt(final String message, final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
           RSA rsa = new RSA(keyTag);
           String encodedMessage = rsa.encrypt(message);
           promise.resolve(encodedMessage);
-      } catch(Exception e) {
+        } catch (Exception e) {
           promise.reject("Error", e.getMessage());
+        }
       }
+    });
   }
 
   @ReactMethod
-  public void encrypt64(String message, String keyTag, Promise promise)  {
-
-      try {
+  public void encrypt64(final String message, final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
           RSA rsa = new RSA(keyTag);
           String encodedMessage = rsa.encrypt64(message);
           promise.resolve(encodedMessage);
-      } catch(Exception e) {
+        } catch (Exception e) {
           promise.reject("Error", e.getMessage());
+        }
       }
+    });
   }
 
-
   @ReactMethod
-  public void decrypt(String encodedMessage, String keyTag, Promise promise)  {
-
-      try {
+  public void decrypt(final String encodedMessage, final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
           RSA rsa = new RSA(keyTag);
           String message = rsa.decrypt(encodedMessage);
           promise.resolve(message);
 
-      } catch(Exception e) {
+        } catch (Exception e) {
           promise.reject("Error", e.getMessage());
+        }
       }
+    });
   }
 
   @ReactMethod
-  public void decrypt64(String encodedMessage, String keyTag, Promise promise)  {
-
-      try {
+  public void decrypt64(final String encodedMessage, final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
           RSA rsa = new RSA(keyTag);
           String message = rsa.decrypt64(encodedMessage);
           promise.resolve(message);
 
-      } catch(Exception e) {
+        } catch (Exception e) {
           promise.reject("Error", e.getMessage());
+        }
       }
-    }
+    });
+  }
 
-    @ReactMethod
-    public void sign(String message, String keyTag, Promise promise)  {
+  @ReactMethod
+  public void sign(final String message, final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          RSA rsa = new RSA(keyTag);
+          String signature = rsa.sign(message);
+          promise.resolve(signature);
+
+        } catch (Exception e) {
+          promise.reject("Error", e.getMessage());
+        }
+      }
+    });
+  }
+
+  @ReactMethod
+  public void sign64(final String message, final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          RSA rsa = new RSA(keyTag);
+          String signature = rsa.sign64(message);
+          promise.resolve(signature);
+
+        } catch (Exception e) {
+          promise.reject("Error", e.getMessage());
+        }
+      }
+    });
+  }
+
+  @ReactMethod
+  public void verify(final String signature, final String message, final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          RSA rsa = new RSA(keyTag);
+          boolean verified = rsa.verify(signature, message);
+          promise.resolve(verified);
+
+        } catch (Exception e) {
+          promise.reject("Error", e.getMessage());
+        }
+      }
+    });
+  }
+
+  @ReactMethod
+  public void verify64(final String signature, final String message, final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          RSA rsa = new RSA(keyTag);
+          boolean verified = rsa.verify64(signature, message);
+          promise.resolve(verified);
+
+        } catch (Exception e) {
+          promise.reject("Error", e.getMessage());
+        }
+      }
+    });
+  }
+
+  @ReactMethod
+  public void getPublicKey(final String keyTag, final Promise promise) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        WritableNativeMap keys = new WritableNativeMap();
 
         try {
-            RSA rsa = new RSA(keyTag);
-            String signature = rsa.sign(message);
-            promise.resolve(signature);
-
-        } catch(Exception e) {
-            promise.reject("Error", e.getMessage());
-        }
-    }
-
-    @ReactMethod
-    public void sign64(String message, String keyTag, Promise promise)  {
-
-        try {
-            RSA rsa = new RSA(keyTag);
-            String signature = rsa.sign64(message);
-            promise.resolve(signature);
-
-        } catch(Exception e) {
-            promise.reject("Error", e.getMessage());
-        }
-    }
-
-    @ReactMethod
-    public void verify(String signature, String message, String keyTag, Promise promise)  {
-
-        try {
-            RSA rsa = new RSA(keyTag);
-            boolean verified = rsa.verify(signature, message);
-            promise.resolve(verified);
-
-        } catch(Exception e) {
-            promise.reject("Error", e.getMessage());
-        }
-    }
-
-    @ReactMethod
-    public void verify64(String signature, String message, String keyTag, Promise promise)  {
-
-        try {
-            RSA rsa = new RSA(keyTag);
-            boolean verified = rsa.verify64(signature, message);
-            promise.resolve(verified);
-
-        } catch(Exception e) {
-            promise.reject("Error", e.getMessage());
-        }
-    }
-
-    @ReactMethod
-    public void getPublicKey(String keyTag, Promise promise)  {
-      WritableNativeMap keys = new WritableNativeMap();
-
-      try {
           RSA rsa = new RSA(keyTag);
           String publicKey = rsa.getPublicKey();
           if (publicKey != null) {
@@ -175,8 +222,10 @@ public class RNRSAKeychainModule extends ReactContextBaseJavaModule {
           } else {
             promise.reject("Error", "Missing public key for that keyTag");
           }
-      } catch(Exception e) {
-        promise.reject("Error", e.getMessage());
+        } catch (Exception e) {
+          promise.reject("Error", e.getMessage());
+        }
       }
-    }
+    });
+  }
 }
