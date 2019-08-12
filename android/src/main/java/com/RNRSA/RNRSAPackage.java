17024ed3b747739e49701acc4b6f4fbd25419d18
Amitay Molko <amitaymolko@gmail.com>
2019-08-12 07:40:43 +0300

    fix(android): fix for RN 44+ by removing @Override Release 1.1.1

--- android/src/main/java/com/RNRSA/RNRSAPackage.java
+++ android/src/main/java/com/RNRSA/RNRSAPackage.java
@@ -12,17 +12,14 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNRSAPackage implements ReactPackage {
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNRSAModule(reactContext), new RNRSAKeychainModule((reactContext)));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
