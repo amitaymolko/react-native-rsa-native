72bd66d10b4dfa9cda3aac174243015f70a21dc9
saeedkargosha <saeedkargosha@gmail.com>
2020-01-23 10:52:58 +0330

    Add sign and verify function for declare module

--- index.d.ts
+++ index.d.ts
@@ -20,7 +20,7 @@ declare module 'react-native-rsa-native' {
 	
 
 	namespace RSA {
-		export function generate(keySize: number): Promise<PublicKey>;
+		export function generate(): Promise<PublicKey>;
 		export function generateKeys(keySize: number): Promise<KeyPair>;
 		export function encrypt(data: string, key: string): Promise<string>;
 		export function decrypt(data: string, key: string): Promise<string>;
@@ -28,9 +28,11 @@ declare module 'react-native-rsa-native' {
 		export function decrypt64(data: string, key: string): Promise<string>;
 		export function sign(data: string, key: string): Promise<string>;
 		export function signWithAlgorithm(data: string, key: string, signature?: TypeCrypto): Promise<string>;
+		export function sign64(data: string, key: string): Promise<string>;
 		export function sign64WithAlgorithm(data: string, key: string, signature?: TypeCrypto): Promise<string>;
 		export function verify(data: string, secretToVerify: string, key: string): Promise<boolean>;
 		export function verifyWithAlgorithm(data: string, secretToVerify: string, key: string, signature?: TypeCrypto): Promise<boolean>;
+		export function verify64(data: string, secretToVerify: string, key: string): Promise<boolean>;
 		export function verify64WithAlgorithm(data: string, secretToVerify: string, key: string, signature?: TypeCrypto): Promise<boolean>;
 		export const SHA256withRSA: string;
 		export const SHA512withRSA: string;
@@ -41,7 +43,9 @@ declare module 'react-native-rsa-native' {
 	}
 
 	namespace RSAKeychain {
-		export function generate(keyTag: string, keySize: number): Promise<PublicKey>;
+		export function generate(keyTag: string): Promise<PublicKey>;
+		export function generateEC(keyTag: string): Promise<PublicKey>;
+		export function generateCSR(keyTag: string, CN: string, signature?: TypeCrypto): Promise<CSRKey>;
 		export function generateKeys(keyTag: string, keySize: number): Promise<PublicKey>;
 		export function generateCSRWithEC(cn: String,keyTag: string, keySize: number): Promise<PublicKey & CSRKey>;
 		export function deletePrivateKey(keyTag: string): Promise<boolean>;
@@ -56,6 +60,8 @@ declare module 'react-native-rsa-native' {
 		export function verifyWithAlgorithm(data: string, secretToVerify: string, keyTag: string, signature?: TypeCrypto): Promise<boolean>;
 		export function verify64WithAlgorithm(data: string, secretToVerify: string, keyTag: string, signature?: TypeCrypto): Promise<boolean>;
 		export function getPublicKey(keyTag: string): Promise<string | undefined>;
+		export function getPublicKeyDER(keyTag: string): Promise<string | undefined>;
+		export function getPublicKeyRSA(keyTag: string): Promise<string | undefined>;
 		export function deletePrivateKey(keyTag: string): Promise<boolean>;
 		export const SHA256withRSA: string;
 		export const SHA512withRSA: string;
